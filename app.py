6fb65011da91b800b33a9c3336547cab7dde49f1
Akhmad Khudri <khudri@binadarma.ac.id>
2025-09-25 21:40:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -98,4 +98,4 @@ def embed_iframe(src, hide_top_px=100, hide_bottom_px=0, height=800):
 iframe_url = "https://ohara.ai/mini-apps/ffbedfd0-09f1-401a-b69e-80a057134614"
 
 # Panggil fungsi
-embed_iframe(iframe_url, hide_top_px=110, hide_bottom_px = 15, height=800)
+embed_iframe(iframe_url, hide_top_px=110, hide_bottom_px = 80, height=800)
